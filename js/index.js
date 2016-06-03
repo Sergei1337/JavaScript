2e3ad30ca1d2510be8f00374125abfb9ece90555
f245 <f245@ivkhk.local>
2016-06-03 11:26:17 +0300

    сделать волидацию логина и пароля при нажатии на кнопку

---
+++ js/index.js
@@ -0,0 +1,12 @@
+alert( 'Привет, Мир!' );
+document.getElementById('new').innerHTML="И тут тоже :P";
+document.getElementById('ser').innerHTML="Опа, текст изменился :o";
+
+document.getElementById('my-but').onclick = function(event){
+	
+};
+function showMessage() {
+	
+}
+
+showMessage();
